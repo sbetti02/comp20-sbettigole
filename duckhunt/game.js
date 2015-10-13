4ece7d0aeebe92ace33e8eee3852b08ae8a0f9be
Scott Bettigole <scott.bettigole@tufts.edu>
2015-10-12 23:46:15 -0400

    got the background image working

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,14 @@
-// Replace this with your code...
+function init () {
+	var canvas = document.getElementById("game_canvas");
+	var ctx = canvas.getContext('2d');
+	var image = new Image();
+	image.addEventListener("load", function() {
+		ctx.drawImage(image, 0, 0);
+	})
+	image.src = 'duckhunt-background.gif';
+	//ctx.drawImage(image, 0,0)
+	//ctx.fillStyle = "green";
+	//ctx.fillRect(10, 10, 100, 100);
+	//ctx.drawImage(duckhunt-background.gif);
+	//return;
+};
