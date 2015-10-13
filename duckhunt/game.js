a2135d728413e3db9ad6a8f3f59092bed799c6c3
Scott Bettigole <scott.bettigole@tufts.edu>
2015-10-13 01:39:09 -0400

    Added the 2 birds

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -2,13 +2,12 @@ function init () {
 	var canvas = document.getElementById("game_canvas");
 	var ctx = canvas.getContext('2d');
 	var image = new Image();
+	var image2 = new Image();
 	image.addEventListener("load", function() {
 		ctx.drawImage(image, 0, 0);
-	})
+		ctx.drawImage(image2, 167, 150, 40, 45, 100, 70, 50, 50);
+		ctx.drawImage(image2,  0, 150, 40, 45, 150, 30, 50, 50);
+	});
 	image.src = 'duckhunt-background.gif';
-	//ctx.drawImage(image, 0,0)
-	//ctx.fillStyle = "green";
-	//ctx.fillRect(10, 10, 100, 100);
-	//ctx.drawImage(duckhunt-background.gif);
-	//return;
+	image2.src = 'duckhunt_various_sheet.png';
 };
