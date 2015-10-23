5cf2260bd8a0b2caa040f228f0b937e759e29750
Scott Bettigole <scott.bettigole@tufts.edu>
2015-10-22 22:36:12 -0400

    take 2 trying to get something to work

--- messages/lab.js
+++ messages/lab.js
@@ -1,5 +1,17 @@
 // Your JavaScript goes here...
 parse() {
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', '/Controller/Action');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        alert(xhr.responseText);
+        var json = data.json,
+        	obj = JSON.parse(json);
+        xhr.send(JSON.stringify(myData));
+    }
+	}
 	var json = '{"result":true,"count":1}',
     	obj = JSON.parse(json);
 
