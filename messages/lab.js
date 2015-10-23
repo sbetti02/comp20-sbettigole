479632908f4ff4d01e121655ffc09fe725e2b48d
Scott Bettigole <scott.bettigole@tufts.edu>
2015-10-23 15:58:28 -0400

    making the user display look nicer and fixing up the code

--- messages/lab.js
+++ messages/lab.js
@@ -1,29 +1,15 @@
-// Your JavaScript goes here...
 function parse() {
 
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'data.json');
-	// xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.onreadystatechange = function () {
 	    if (xhr.readyState == 4) {
-	    	//elem = xhr.responseText;
 	    	elem = document.getElementById("messages");
 	    	parsing_json = JSON.parse(xhr.responseText);
-	    	//elem.innerHTML = xhr.responseText;
 	    	for (var i = 0; i < parsing_json.length; i++) {
-	    		elem.innerHTML = elem.innerHTML + '<p>' + parsing_json[i].content + ' ' + parsing_json[i].username + '</p>' + '<br';
+	    		elem.innerHTML = elem.innerHTML + '<span>' + parsing_json[i].content + ' - ' + parsing_json[i].username + '</span>' + '<br>';
 	    	}
-
-	        //alert(xhr.responseText);
-	        //var json = data.json,
-	        //	obj = JSON.parse(json);
-	        //xhr.send(JSON.stringify(myData));
-	    }
+    }
 	}
 	xhr.send();
-
-	//var json = '{"result":true,"count":1}',
-    //	obj = JSON.parse(json);
-
-	//alert(obj.count);
 }
